7eb8c3815c1933cd07a25a689f233adb0a4d3579
front-ed <edwinschutjes@gmail.com>
2023-04-05 22:36:26 +0200

    add title component and update services component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Tours />
       <Footer />
     </>
   );
--- src/data.js
+++ src/data.js
@@ -10,3 +10,24 @@ export const socialLinks = [
   { id: 2, href: "https://www.twitter.com", icon: "fa-brands fa-twitter" },
   { id: 3, href: "https://www.instagram.com", icon: "fa-brands fa-instagram" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    title: "saving money",
+    icon: "fas fa-wallet fa-fw",
+    text: "Forage knausgaard echo park, messenger bag lumbersexual bun. ",
+  },
+  {
+    id: 2,
+    title: "endless hiking",
+    icon: "fas fa-tree fa-fw",
+    text: "Shabby chic flexitarian banh mi 3 wolf moon. Migas listicle bitters.",
+  },
+  {
+    id: 3,
+    title: "amazing comfort",
+    icon: "fas fa-socks fa-fw",
+    text: "Snackwave twee poutine pork belly, tilde activated charcoal mukbang.",
+  },
+];
